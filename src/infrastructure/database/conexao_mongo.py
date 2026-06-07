24ea7d962fd3e88392ea7a3f3f090bf44c94bffd
Maria Bárbara Vieira e Silva <bitavieira.tech@gmail.com>
2026-06-07 18:36:15 -0300

    feature de conexão com o banco

--- src/infrastructure/database/conexao_mongo.py
+++ src/infrastructure/database/conexao_mongo.py
@@ -6,4 +6,17 @@ load_dotenv()
 
 mongo_uri= os.getenv('MONGO_URI')
 client = MongoClient(mongo_uri)
-db = client.zelo
+
+
+try:
+    client.admin.command("ping")
+    print("✔ Conexão com MongoDB: OK")
+except Exception as e:
+    print(" Erro de conexão:", e)
+
+# TESTE 2: listar bancos (opcional)
+print("Bancos disponíveis:")
+print(client.list_database_names())
+
+db = client["zelo-tests"]
+conexao = db.conexao
