8da47acf42d10f819d926dddd4583cc7242a76a3
Matheus Marques <megazord@Matheuss-MacBook-Pro.local>
2026-06-08 23:04:11 -0300

    Configura gunicorn para deploy de produção

--- src/app.py
+++ src/app.py
@@ -62,6 +62,7 @@ def create_app():
         
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=Config.PORT, debug=Config.DEBUG)
